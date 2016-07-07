720d5649ec26129bd0f86b31645be1171c7bc000
Will Tang <willwyatttang@gmail.com>
2016-07-06 17:31:34 -0700

    Node successfully connects to python script

--- src/pyRec/recommendation.py
+++ src/pyRec/recommendation.py
@@ -2,18 +2,7 @@ from numpy import dot, linalg, average, array
 import heapq 
 from math import trunc
 from json import dumps, loads
-from sys import stdin, stdout
-
-# print(dumps(555555555))
-# try: 
-#   lines = stdin.readlines()
-#   print lines
-# except:
-#   print("Unexpected error:", sys.exc_info()[0])
-
 from sys import argv
-print argv
-# print dumps(stdin)
 
 class NLargest(list):
   def __init__(self, size):
@@ -94,20 +83,14 @@ class Recommendation:
       results[user] = self.recommend(user)
     return results
 
+def recommend(data):
+  recs = Recommendation(data, 5, 5);
+  print (dumps(recs.allRecommendations()))
 
-dataSet = {
-"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
-"Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
-"Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
-"Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
-"Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
-"Jordyn": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
-"Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
-"Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
-}
+print (recommend(loads(argv[1])));
 
-# a = Recommendation(dataSet, 5, 3)
-# print (dumps(a.allRecommendations()))
+
+
 
 
 
--- src/rec-int/py-int.js
+++ src/rec-int/py-int.js
@@ -1,6 +1,28 @@
 const PythonShell = require('python-shell');
 
-const py = new PythonShell('recommendation.py', { mode: 'text', scriptPath: `${__dirname}/../pyRec/`, args: ['value1', 'value2'] });
+const testData = {
+  "Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
+  "Bill": {"Blues Traveler": 2.0, "Broken Bells": 3.5, "Deadmau5": 4.0, "Phoenix": 2.0, "Slightly Stoopid": 3.5, "Vampire Weekend": 3.0},
+  "Chan": {"Blues Traveler": 5.0, "Broken Bells": 1.0, "Deadmau5": 1.0, "Norah Jones": 3.0, "Phoenix": 5, "Slightly Stoopid": 1.0},
+  "Dan": {"Blues Traveler": 3.0, "Broken Bells": 4.0, "Deadmau5": 4.5, "Phoenix": 3.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 2.0},
+  "Hailey": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
+  "Jordyn": {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
+  "Sam": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
+  "Veronica": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+};
+
+const pyConnect = function(json) {
+  const pythonConfig = {
+    mode: 'text',
+    scriptPath: `${__dirname}/../pyRec/`,
+    args: [JSON.stringify(json)],
+    pythonPath: '/Library/Frameworks/Python.framework/Versions/3.5/bin/python3',
+  };
+  const pyConnection = new PythonShell('recommendation.py', pythonConfig);
+  pyConnection.on('message', message => console.log(message));
+};
+
+pyConnect(testData);
 
 
 
